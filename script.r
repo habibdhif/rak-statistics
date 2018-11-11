2866181c79670975c8e79d7ba244d9e486f22e56
fguibourt <44946638+fguibourt@users.noreply.github.com>
2018-11-11 18:10:39 +0100

    Update script.r
    Graphiques : 
        - ajout d'un titre pour les barplots
        - organisation en catégorie de variables
        - ajout des histogrammes pour les variables quantitatives continues
    Stats univariées : 
        - ajout des résumés stats pour les variables quanti
        - ajout du coeff d'assymétrie pour les variables quanti

--- script.r
+++ script.r
@@ -20,16 +20,20 @@ getwd()
 
 ## Reading and Preparing data (the na values where kept as they are, it's up to you to handle them as you see best)
 data <- read.csv("data.csv",sep = ",")
+attach(data)
+
 # Redefining column names
-new_column_names <- c("submitted_date","weekly_freq","last_taken","last_thrown","average_taken","average_thrown","weekly_consumption_out","sorting_waste","sign_seen","sign_influence","physical_form","fried","sauce","eat_with","sport_hours","status","cloud_cover","wind_speed","tempreature","precipetation","sign","gender","age","meal")
+new_column_names <- c("submitted_date","weekly_freq","last_taken","last_thrown","average_taken","average_thrown","weekly_consumption_out","sorting_waste","sign_seen","sign_influence","physical_form","fried","sauce","eat_with","sport_hours","status","cloud_cover","wind_speed","temperature","precipetation","sign","gender","age","meal")
 colnames(data) <- new_column_names
+
 # Deleting the "sign_influence" column 
-data <- data[,c(1:9,11:ncol(data))]
+data <- data[,-10]
 
 library(plyr)
 # Remapping values
 data$sorting_waste <- mapvalues(data$sorting_waste,from=c("Jamais","Moins d'une fois sur deux","Plus d'une fois sur deux", "Toujours"),to=c("0","0-0.5","0.5-1","1"))
 data$age <- mapvalues(data$age,from=c("Moins de 30 ans","Entre 30 et 40","Entre 40 et 50", "Plus de 50 ans"),to=c("<30","30-40","40-50",">50"))
+
 # Converting values that contain comma decimal seperators (can't be interpreted) to integer
 # using the get_ciel method defined earlier
 data$last_thrown <- sapply(data$last_thrown,FUN = get_ceil)
@@ -41,30 +45,67 @@ data$sport_hours <- sapply(data$sport_hours,FUN = get_ceil)
 
 ## Plotting data
 # Again na values where not handled, handle them as you wish 
-barplot(table(data$weekly_freq))
-barplot(table(data$last_taken))
-barplot(table(data$last_thrown))
-barplot(table(data$average_taken))
-barplot(table(data$average_thrown))
-barplot(table(data$weekly_consumption_out))
+# Discrete quantitative variables
+barplot(table(data$weekly_freq), main = "barplot RAK weekly frequency")
+barplot(table(data$last_taken), main = "barplot nb BREAD last taken")
+barplot(table(data$last_thrown), main = "barplot nb BREAD last thrown")
+barplot(table(data$average_taken), main = "barplot nb BREAD avg taken")
+barplot(table(data$average_thrown), main = "barplot nb BREAD avg thrown")
+barplot(table(data$weekly_consumption_out), main = "barplot nb BREAD weekly consumption outside rak")
 
+# Ordinal quantitative variables
+barplot(table(data$physical_form), main = "barplot PHYSICAL form")
+
+# Nominal qualitative variables
 pie(table(data$sorting_waste),labels = get_labels(data$sorting_waste))
 pie(table(data$sign_seen),labels = get_labels(data$sign_seen))
-pie(table(data$physical_form),labels = get_labels(data$physical_form))
 pie(table(data$fried),labels = get_labels(data$fried))
 pie(table(data$sauce),labels = get_labels(data$sauce))
-
-barplot(table(data$wind_speed))
-barplot(table(data$sport_hours))
-
 pie(table(data$status),labels = get_labels(data$status))
 pie(table(data$cloud_cover),labels = get_labels(data$cloud_cover))
-
-barplot(table(data$wind_speed))
-barplot(table(data$tempreature))
-barplot(table(data$precipetation))
-
 pie(table(data$sign),labels = get_labels(data$sign))
 pie(table(data$gender),labels = get_labels(data$gender))
 pie(table(data$age),labels = get_labels(data$age))
 pie(table(data$meal),labels = get_labels(data$meal))
+
+# Continuous quantitative variables (good to have histograms for those)
+barplot(table(data$wind_speed))
+hist(data$wind_speed)
+barplot(table(data$sport_hours))
+hist(data$sport_hours)
+barplot(table(data$temperature))
+hist(data$temperature)
+barplot(table(data$precipetation))
+hist(data$precipetation)
+
+
+# Summaries for quantitative data : position & dispersion indicators
+summary(data$weekly_freq)
+summary(data$last_taken)
+summary(data$last_thrown)
+summary(data$average_taken)
+summary(data$average_thrown)
+summary(data$weekly_consumption_out)
+summary(data$physical_form)
+summary(data$eat_with)
+summary(data$sport_hours)
+summary(data$wind_speed)
+summary(data$sport_hours)
+summary(data$temperature)
+summary(data$precipetation)
+
+# skewness : if <0 <=> skewed towards the left, if > 0, skewed towards the right
+library("e1071")
+skewness(data$weekly_freq, na.rm = T)
+skewness(data$last_taken, na.rm = T)
+skewness(data$last_thrown, na.rm = T)
+skewness(data$average_taken, na.rm = T)
+skewness(data$average_thrown, na.rm = T)
+skewness(data$weekly_consumption_out, na.rm = T)
+skewness(data$physical_form, na.rm = T)
+skewness(data$eat_with, na.rm = T)
+skewness(data$sport_hours, na.rm = T)
+skewness(data$wind_speed, na.rm = T)
+skewness(data$sport_hours, na.rm = T)
+skewness(data$temperature, na.rm = T)
+skewness(data$precipetation, na.rm = T)
